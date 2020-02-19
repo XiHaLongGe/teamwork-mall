d1bab1ce8ce3d30a58b7e36401dca1ad03b9c26c
Long Ji Peng <1530937232@qq.com>
2020-02-19 15:08:44 +0800

    service

---
+++ src/main/java/com/nf/service/UserService.java
@@ -0,0 +1,13 @@
+package com.nf.service;
+
+/**
+ * @Author: LJP
+ * @Classname UserService
+ * @Date: 2020-02-19 15:07
+ * @Description:
+ */
+public class UserService {
+    public void insert(){
+        System.out.println("UserService...............insert................");
+    }
+}
